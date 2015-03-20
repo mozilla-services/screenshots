818df322bde5cd70ae4a9fd059e78a9de31d47c3
Ian Bicking <ianb@colorstudy.com>
2015-03-20 17:26:45 -0500

    Include deployment/setup scripts we've written

---
+++ bin/start-server.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+set -e
+
+cd /home/ubuntu/pageshot
+export DB_HOST=pageshot.czvvrkdqhklf.us-east-1.rds.amazonaws.com
+export DB_USER=pageshot
+export DB_PASS=pageshot
+NODE=/home/ubuntu/.nvm/versions/node/v0.12.0/bin/node
+
+exec $NODE ./run &>> /home/ubuntu/pageshot.logs
---
+++ bin/update-source.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+set -e
+
+if [ ! -e .git ] ; then
+  echo "Error: $0 must be run from the root of the pageshot checkout"
+  exit 3
+fi
+
+git pull
+cd server
+npm install
+gulp transforms
+if [ -e ./pageshot.xpi ] ; then
+  echo "Updating XPI"
+  if [ ! -e ./pageshot.update.rdf ] ; then
+    echo "Missing pageshot.update.rdf"
+    exit 2
+  fi
+  mv ./pageshot.xpi dist/
+  mv ./pageshot.update.rdf dist/
+fi
+mv dist-production dist-production.obsolete
+mv dist dist-production
+rm -r dist-production.obsolete
+sudo service pageshot restart
