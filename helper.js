b881ab248dc16919f339a55beebd9f1c029e9cf5
Ian Bicking <ianb@colorstudy.com>
2015-04-15 17:51:57 -0500

    Save addon output to addon.log (so you can tail it) Play some little sounds as the rebuilding happens.

--- helper.js
+++ helper.js
@@ -1,13 +1,11 @@
-
+/*jshint node:true */
 
 console.log("Addon transformation complete.");
 
-var output = require("child_process").spawnSync("bash", ["-c", "cd addon/dist && ./run --local"]).output;
+var output = require("child_process").spawnSync("bash", ["-c", "cd addon/dist && ./run --local > ../../addon.log"]).output;
 
 for (var i in output) {
   if (output[i]) {
     console.log(output[i].toString());
   }
 }
-
-
