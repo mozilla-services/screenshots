886c678719fac1ff0911f50fa208702ebe1d223d
Ian Bicking <ianb@colorstudy.com>
2015-04-28 11:30:13 -0500

    Make comments work: - Keep in-progress comments in local component state - Still send comment when Enter is hit - Add updateComment method to Shot and Clip

--- addon/lib/shot.js
+++ addon/lib/shot.js
@@ -365,6 +365,11 @@ class AbstractShot {
     this._comments.push(comment);
     this._dirty("comments");
   }
+  updateComment(index, json) {
+    let comment = new this._shot.Comment(json);
+    this._comments[index] = comment;
+    this._dirty("comments");
+  }
   // FIXME: no delete, nor comment editing
 
   get ogTitle() {
@@ -715,6 +720,11 @@ class _Clip {
   addComment(json) {
     let comment = new this._shot.Comment(json);
     this._comments.push(comment);
+    this._dirty("comments");
+  }
+  updateComment(index, json) {
+    let comment = new this._shot.Comment(json);
+    this._comments[index] = comment;
     this._dirty("comments");
   }
 
