7f8e1985f12f0c380658aa656067d56ffe71f1e8
Ian Bicking <ianb@colorstudy.com>
2015-05-05 00:06:53 -0500

    There are more of us!

--- server/src/views/main.js
+++ server/src/views/main.js
@@ -4,13 +4,15 @@ exports.Main = React.createClass({
   render: function () {
     return (
       <div className="frontpage">
-        <h1>Pageshot</h1>
+        <h1>Pageshot <em>alpha</em></h1>
 
         <p>
           PageShot is an experiment
           by <a href="https://blog.mozilla.org/services/">Mozilla Cloud
-          Services</a> and <a href="mailto:ibicking@mozilla.com">Ian
-          Bicking</a>.  It will take shots of the webpage you are on -
+          Services</a> and: <a href="mailto:ibicking@mozilla.com">Ian
+          Bicking</a>, <a href="http://www.brampitoyo.com/">Bram Pitoyo</a>,
+          and <a href="http://donovanpreston.blogspot.com/">Donovan Preston</a>.
+          It takes shots of the webpage you are on -
           creating a static and stable version of the page that you can
           share with anyone.  The original page can disappear, you can
           take shots of dynamic web applications, you can take shots of
