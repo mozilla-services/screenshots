2370d41916de98e424516bbdb1dd0bc1839b48de
Ian Bicking <ianb@colorstudy.com>
2015-07-30 15:32:42 -0500

    Always define ga() (with a stub when necessary), so we can use it with impunity elsewhere

--- server/src/views/shell.js
+++ server/src/views/shell.js
@@ -13,12 +13,13 @@ export class Shell extends React.Component {
     }
     let gaScript = null;
     let gaCode = null;
+    let gaJs;
     if (this.props.gaId) {
       if (this.props.gaId.search(/^[a-z0-9\-]+$/i) === -1) {
         // Doesn't look like a valid code
         console.warn("Invalid Google Analytics code:", this.props.gaId);
       }
-      let gaJs = `
+      gaJs = `
        (function () {
          window.GoogleAnalyticsObject = "ga";
          window.ga = window.ga || function () {
@@ -30,9 +31,14 @@ export class Shell extends React.Component {
        })();
       `;
       gaScript = <script src="//www.google-analytics.com/analytics.js" async="1"></script>;
-      gaCode = <script dangerouslySetInnerHTML={{__html: gaJs}}></script>;
+    } else {
+      gaJs = `
+      window.ga = function () {
+        console.info.apply(console, ["stubbed ga("].concat(arguments).concat([")"]));
+      };
+      `;
     }
-
+    gaCode = <script dangerouslySetInnerHTML={{__html: gaJs}}></script>;
     return <html prefix="og: http://ogp.me/ns#">
       <head>
         <title>{this.props.title}</title>
