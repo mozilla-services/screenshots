76ad640b71f04abb2ea802ceed683181a1b9a3d4
Ian Bicking <ianb@colorstudy.com>
2015-08-03 11:49:22 -0500

    Update paths of start-server to pick up .env.production consistently

--- bin/start-server.sh
+++ bin/start-server.sh
@@ -2,10 +2,10 @@
 
 set -e
 
-cd /home/ubuntu/pageshot/server
+cd /home/ubuntu/pageshot/
 export DB_HOST=pageshot.czvvrkdqhklf.us-east-1.rds.amazonaws.com
 export DB_USER=pageshot
 export DB_PASS=pageshot
 NODE=/home/ubuntu/.nvm/versions/node/v0.12.0/bin/node
 
-exec $NODE ./run &>> /home/ubuntu/pageshot.logs
+exec $NODE ./server/run &>> /home/ubuntu/pageshot.logs
