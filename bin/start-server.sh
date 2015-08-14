478eff751cb715c30c6ce3c584104ccea883f9c6
Ian Bicking <ianb@colorstudy.com>
2015-08-14 13:09:16 -0500

    Use production path

--- bin/start-server.sh
+++ bin/start-server.sh
@@ -12,4 +12,4 @@ export NODE_ENV=production
 
 NODE=/home/ubuntu/.nvm/versions/node/v0.12.0/bin/node
 
-exec $NODE -e 'require("babel/polyfill"); require("./build/server/server");' &>> /home/ubuntu/pageshot.logs
+exec $NODE -e 'require("babel/polyfill"); require("./build-production/server/server");' &>> /home/ubuntu/pageshot.logs
