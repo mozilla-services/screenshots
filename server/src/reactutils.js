adfebec065543e93b6788e37bd94405c9786d94f
Ian Bicking <ianb@colorstudy.com>
2015-09-14 11:48:37 -0500

    Make addScript argument optional; export a generic extend function

--- server/src/reactutils.js
+++ server/src/reactutils.js
@@ -1,12 +1,26 @@
 exports.addReactScripts = function (body, addScript) {
-  let footer = "</body></html>";
-  let frontmatter = body.slice(0, body.length - footer.length);
-  addScript = addScript.replace(/<script/ig, "\\x3cscript").replace(/<\/script/ig, "\\x3c/script");
-  return (`<!DOCTYPE html>
-${frontmatter}
-<script>
-${addScript}
-</script>
-${footer}`
-  );
+  if (addScript) {
+    let footer = "</body></html>";
+    let frontmatter = body.slice(0, body.length - footer.length);
+    addScript = addScript.replace(/<script/ig, "\\x3cscript").replace(/<\/script/ig, "\\x3c/script");
+    return (`<!DOCTYPE html>
+  ${frontmatter}
+  <script>
+  ${addScript}
+  </script>
+  ${footer}`
+    );
+  } else {
+    return `<!DOCTYPE html>\n${body}`;
+  }
+};
+
+exports.extend = function (...objs) {
+  let obj = {};
+  for (let extender of objs) {
+    for (let attr in extender) {
+      obj[attr] = extender[attr];
+    }
+  }
+  return obj;
 };
