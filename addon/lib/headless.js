3ab54e90d62af8eb971aa79885984d70c3af9cf5
Ben Ennis <ben@outernet.is>
2015-12-15 11:09:23 -0800

    added readable request handler

--- addon/lib/headless.js
+++ addon/lib/headless.js
@@ -34,22 +34,59 @@ exports.init = function init(prefs) {
   var server = new nsHttpServer();
   server.start(port);
   server.registerPathHandler('/', handleRequest);
-  console.info("headless server listening on PORT".replace('PORT', port));
+  server.registerPathHandler('/readable/', handleReadableRequest);
 };
 
-function handleRequest(request, response)
-{
-  response.setStatusLine("1.1", 200, "OK");
+function sleep(milliseconds) {
+  var start = new Date().getTime();
+  for (var i = 0; i < 1e7; i++) {
+    if ((new Date().getTime() - start) > milliseconds){
+      break;
+    }
+  }
+}
 
-  var url = request._queryString;
-  var backendUrl = randomString(RANDOM_STRING_LENGTH) + "/" + urlDomainForId(url);
-  console.log('recieved request: URL -> BACK'
-                .replace('URL', url)
-                .replace('BACK', backendUrl)
-              );
+function handleReadableRequest(request, response) {
+  var fired = false;        // Assign local variable fired, because onload gets tricky
+  response.processAsync();  // Tell response handler this is going to take a while
+  var url = request._queryString; // Grab the passed URL 
+  var backendUrl = randomString(RANDOM_STRING_LENGTH) + "/" + urlDomainForId(url); // Create backend url, usually happens in shooter.js
+  console.log('recieved request: URL -> BACK'   // Log both URLs
+                .replace('URL', url).replace('BACK', backendUrl));
 
-  response.write(backendUrl);
-  processTab(url, backendUrl);
+  tabs.open({   // Open a tab
+    "url": url,
+    "onOpen": function (tab) {  // When the tab opens, run this on it
+      tab.on("load", function (tab) { 
+        if (!fired) {           // If it hasn't fired, click on readable button
+          fired = true;
+          var wm = Cc["@mozilla.org/appshell/window-mediator;1"]
+                             .getService(Ci.nsIWindowMediator);
+          var browserWindow = wm.getMostRecentWindow("navigator:browser");
+          browserWindow.document.getElementById("reader-mode-button").click();
+        } else {                // If it has fired, remove the toolbar
+          var worker = tab.attach({
+            contentScript:
+              'var toolbar = document.getElementById("reader-toolbar");\n' +
+              'toolbar.parentElement.removeChild(toolbar);\n' +
+              'self.port.emit("done");'
+          });
+          worker.port.on("done", function () { // When the worker emits done, wait a bit then fire autoshot
+            console.log('readability should be finished');
+            sleep(500000);
+            autoShot(tab, backend, backendUrl);
+          });
+        }
+      });
+
+      tab.on("close", function () {     // When the tab is closed we know autoshot is done, so return the URL
+        console.log('completed processing BACK'.replace('BACK', backendUrl));
+        response.setStatusLine("1.1", 200, "OK");
+        response.write(contentServerString + backendUrl + '\n');
+        response.finish();
+      });
+    }
+  });
 }
 
 function handleRequest(request, response) {
