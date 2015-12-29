2134eddfe7e30d86e6b86c30cc0eb334986faa3a
Ian Bicking <ianb@colorstudy.com>
2015-12-29 14:29:05 -0500

    Add a management page (WIP) to /admin/ Add a new framework for setting up isomorphic react pages

---
+++ server/src/reactrender.js
@@ -0,0 +1,36 @@
+const { addReactScripts } = require("./reactutils");
+const React = require("react");
+const { getGitRevision } = require("./linker");
+
+exports.render = function (req, res, page) {
+  console.log("rendering");
+  let modelModule = require("./" + page.modelModuleName);
+  let viewModule = require("./" + page.viewModuleName);
+  modelModule.createModel(req).then((model) => {
+    model = {
+      backend: req.backend,
+      gitRevision: getGitRevision(),
+      ...model
+    };
+    let serverModel = {
+      staticLink: req.staticLink,
+      ...model
+    };
+    let head = React.renderToStaticMarkup(viewModule.HeadFactory(serverModel));
+    let body = React.renderToString(viewModule.BodyFactory(serverModel));
+    let doc = `
+    <html>
+      ${head}
+      <body>
+        <div id="react-body-container">
+          ${body}
+        </div>
+      </body></html>
+    `.trim();
+    // FIXME: we should just inline the addReactScripts functionality in this function:
+    doc = addReactScripts(doc, `controller.launch(${JSON.stringify(model)});`);
+    res.send(doc);
+  }).catch((err) => {
+    res.type("txt").status(500).send("Error: " + err + "\n" + err.stack);
+  });
+};
