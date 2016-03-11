eb16ac7509b2cd7f46912ad87274bf37c164f8c4
Ian Bicking <ianb@colorstudy.com>
2016-03-10 16:44:37 -0800

    Clean up some unused routines in helper/viewerworker

--- addon/lib/helperworker.js
+++ addon/lib/helperworker.js
@@ -107,13 +107,8 @@ function resetPageMod(backend) {
         });
       }));
 
-      worker.port.on("requestScreenshot", watchFunction(function (info) {
-        captureTab(worker.tab, info).then(function (image) {
-          worker.port.emit("screenshot", image, info);
-        }, function (error) {
-          console.error("Got error taking screenshot:", error);
-        });
-      }));
+      // FIXME: we aren't using this, but it does allow richer clipboard access
+      // than the normal DOM API
       worker.port.on("requestClipboard", watchFunction(function (info) {
         if (info.text) {
           clipboard.set(info.text, "text");
@@ -132,37 +127,7 @@ function resetPageMod(backend) {
           });
         }
       }));
-      // Checks if a page has been modified by doing a conditional request
-      // (doesn't work very well in practice, most sites don't return 304 on
-      // content pages)
-      worker.port.on("checkCaptured", watchFunction(function (pageInfo) {
-        var captured = pageInfo.captured;
-        var path = pageInfo.path;
-        var url = pageInfo.url;
-        var req = new XMLHttpRequest();
-        // This scratch URL stuff is because the browser is fulfilling
-        // requests with a 200 response even when it gets a 304
-        // upstream, even though it is not supposed to do so when a
-        // cache header is explicitly set.
-        var scratchUrl = url;
-        if (url.indexOf('?') == -1) {
-          scratchUrl += '?';
-        } else {
-          scratchUrl += '&';
-        }
-        scratchUrl += '_cachebreak=' + Math.random();
-        req.open("HEAD", scratchUrl);
-        req.setRequestHeader("If-Modified-Since", (new Date(captured)).toUTCString());
-        req.onload = function () {
-          worker.port.emit("checkCapturedResult", {
-            path: path,
-            url: url,
-            captured: captured,
-            status: req.status
-          });
-        };
-        req.send();
-      }));
+
     }
   });
 }
