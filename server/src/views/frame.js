c5a03ab6101fcfe0fc784f8c6b53ea7ba0b0ceb5
Donovan Preston <donovanpreston@gmail.com>
2016-03-15 17:50:44 -0400

    Fixes #1055 Move the reminder up; add an arrow pointing at the share button; don't show the reminder if the shot page is loaded 30 seconds after the shot was taken.

--- server/src/views/frame.js
+++ server/src/views/frame.js
@@ -493,11 +493,22 @@ class Frame extends React.Component {
     }
 
     let isPublic = null;
-    if (this.props.isOwner && !this.state.closePrivacyNotice) {
+    let arrowElement = <div className="arrow-up" />;
+    if (this.props.isOwner && !this.state.closePrivacyNotice && Date.now() - shot.createdDate < 30000) {
       if (shot.isPublic) {
-        isPublic = <span id="private-notice" onClick={ this.onClickPrivacyNotice.bind(this) }>This page is only visible to you until you share the link.</span>;
+        isPublic = <span
+          id="private-notice"
+          onClick={ this.onClickPrivacyNotice.bind(this) }>
+          This page is only visible to you until you share the link.
+          { arrowElement }
+        </span>;
       } else {
-        isPublic = <span id="private-notice" onClick={ this.onClickPrivacyNotice.bind(this) }>You&#39;ve saved your personal version of this page. This page is only visible to you until you share the link.</span>;
+        isPublic = <span
+          id="private-notice"
+          onClick={ this.onClickPrivacyNotice.bind(this) }>
+          You&#39;ve saved your personal version of this page. This page is only visible to you until you share the link.
+          { arrowElement }
+        </span>;
       }
     }
 
