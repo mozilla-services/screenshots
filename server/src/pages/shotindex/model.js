f678d94faa2f9c2fbfd8ab2da4e3062ac749ef62
Ian Bicking <ianb@colorstudy.com>
2016-08-15 18:21:09 -0500

    Fix #1113, proxy favicon and images in shot index page Also remove the mozlog aliasing of console.log, as 'log' isn't a proper mozlog level Allow images from contentOrigin, without the port stripped (necessary for serving from localhost:10081)

--- server/src/pages/shotindex/model.js
+++ server/src/pages/shotindex/model.js
@@ -1,9 +1,19 @@
+const { createProxyUrl } = require("../../proxy-url");
+
 exports.createModel = function (req) {
   let serverModel = {
     title: "My Shots",
     defaultSearch: req.query.q || null
   };
   serverModel.shots = req.shots;
+  for (let shot of req.shots) {
+    if (shot.favicon) {
+      shot.favicon = createProxyUrl(req, shot.favicon);
+    }
+    for (let image of (shot.images || [])) {
+      image.url = createProxyUrl(req, image.url);
+    }
+  }
   let jsonModel = Object.assign(
     {},
     serverModel,
