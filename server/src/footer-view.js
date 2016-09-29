d76e8e76b66d7c5a42dece12c40a5a62f5ed090e
Peter deHaan <peter@deseloper.com>
2016-09-29 10:34:13 -0700

    Fix typo for infringement link in footer
    Fixes #1619

--- server/src/footer-view.js
+++ server/src/footer-view.js
@@ -8,7 +8,7 @@ exports.Footer = class Footer extends React.Component {
           <div className="legal-links">
           <a href="/terms">Terms</a>
           <a href="/privacy">Privacy Notice</a>
-          <a href="https://www.mozilla.org/en-US/about/legal/report-infringement/" target="_blank">Report IP Infrigement</a>
+          <a href="https://www.mozilla.org/en-US/about/legal/report-infringement/" target="_blank">Report IP Infringement</a>
           <a href={ `mailto:pageshot-feedback@mozilla.com?subject=Page%20Shot%20Feedback&body=Feedback%20regarding:%20${this.props.forUrl}` } target="_blank">Send Feedback</a>
           <a href="/leave-page-shot">Leave Page Shot</a>
           </div>
