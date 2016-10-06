00a3661522299f84b7dc5ec989fa4fa993d47d0a
Ian Bicking <ianb@colorstudy.com>
2016-10-06 12:22:42 -0500

    Add noAnalytics property to suppress GA on a page. Fix #1708, remove GA from the creating page

--- server/src/pages/creating/model.js
+++ server/src/pages/creating/model.js
@@ -1,6 +1,7 @@
 exports.createModel = function (req) {
   let finishedUrl = `/${encodeURIComponent(req.params.id)}/${encodeURIComponent(req.params.domain)}`;
   let model = {
+    noAnalytics: true,
     title: `Creating ${req.query.title}`,
     docTitle: req.query.title,
     docUrl: req.query.url,
