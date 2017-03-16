39830304b38a70c4a60494f8f58b097f17136118
Diana Thayer <me@dianathayer.org>
2017-03-15 21:06:00 -0700

    use window.crypto.getRandomValues

--- addon/webextension/makeUuid.js
+++ addon/webextension/makeUuid.js
@@ -1,12 +1,47 @@
 window.makeUuid = (function () {
 
-  // From http://stackoverflow.com/questions/105034/create-guid-uuid-in-javascript
+  // From http://pastebin.com/JSgC7eej
   return function makeUuid() { // eslint-disable-line no-unused-vars
-    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-      var r = Math.random()*16 | 0,
-        v = c === 'x' ? r : (r & 0x3 | 0x8);
-      return v.toString(16);
-    });
+    // get sixteen unsigned 8 bit random values
+    var u = window
+      .crypto
+      .getRandomValues(new Uint8Array(16));
+
+    // set the version bit to v4
+    u[6] = (u[6] & 0x0f) | 0x40
+
+    // set the variant bit to "don't care" (yes, the RFC
+    // calls it that)
+    u[8] = (u[8] & 0xbf) | 0x80
+
+    // hex encode them and add the dashes
+    var uid = "";
+    uid += u[0].toString(16);
+    uid += u[1].toString(16);
+    uid += u[2].toString(16);
+    uid += u[3].toString(16);
+    uid += "-";
+
+    uid += u[4].toString(16);
+    uid += u[5].toString(16);
+    uid += "-";
+
+    uid += u[6].toString(16);
+    uid += u[7].toString(16);
+    uid += "-";
+
+    uid += u[8].toString(16);
+    uid += u[9].toString(16);
+    uid += "-";
+
+    uid += u[10].toString(16);
+    uid += u[11].toString(16);
+    uid += u[12].toString(16);
+    uid += u[13].toString(16);
+    uid += u[14].toString(16);
+    uid += u[15].toString(16);
+
+    return uid;
   };
 })();
 null;
