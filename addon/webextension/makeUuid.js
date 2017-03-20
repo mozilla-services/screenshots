2f7688f4fb58f3143a0b00902754ebe8a742e029
Diana Thayer <me@dianathayer.org>
2017-03-19 20:19:56 -0700

    revert shared/shot.js, update and correct makeUuid.js

--- addon/webextension/makeUuid.js
+++ addon/webextension/makeUuid.js
@@ -1,47 +1,17 @@
 window.makeUuid = (function () {
 
-  // From http://pastebin.com/JSgC7eej
+  // generates a v4 UUID
   return function makeUuid() { // eslint-disable-line no-unused-vars
     // get sixteen unsigned 8 bit random values
-    var u = window
+    var randomValues = window
       .crypto
-      .getRandomValues(new Uint8Array(16));
+      .getRandomValues(new Uint8Array(36));
 
-    // set the version bit to v4
-    u[6] = (u[6] & 0x0f) | 0x40
-
-    // set the variant bit to "don't care" (yes, the RFC
-    // calls it that)
-    u[8] = (u[8] & 0xbf) | 0x80
-
-    // hex encode them and add the dashes
-    var uid = "";
-    uid += u[0].toString(16);
-    uid += u[1].toString(16);
-    uid += u[2].toString(16);
-    uid += u[3].toString(16);
-    uid += "-";
-
-    uid += u[4].toString(16);
-    uid += u[5].toString(16);
-    uid += "-";
-
-    uid += u[6].toString(16);
-    uid += u[7].toString(16);
-    uid += "-";
-
-    uid += u[8].toString(16);
-    uid += u[9].toString(16);
-    uid += "-";
-
-    uid += u[10].toString(16);
-    uid += u[11].toString(16);
-    uid += u[12].toString(16);
-    uid += u[13].toString(16);
-    uid += u[14].toString(16);
-    uid += u[15].toString(16);
-
-    return uid;
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+      var i = Array.prototype.slice.call(arguments).slice(-2)[0]; // grab the `offset` parameter
+      var r = randomValues[i]%16|0, v = c === 'x' ? r : (r&0x3|0x8);
+      return v.toString(16);
+    });
   };
 })();
 null;
--- shared/shot.js
+++ shared/shot.js
@@ -145,48 +145,20 @@ function deepEqual(a, b) {
   return true;
 }
 
-// From http://pastebin.com/JSgC7eej
 function makeUuid() {
-  // get sixteen unsigned 8 bit random values
-  var u = window
-    .crypto
-    .getRandomValues(new Uint8Array(16));
-
-  // set the version bit to v4
-  u[6] = (u[6] & 0x0f) | 0x40
-
-  // set the variant bit to "don't care" (yes, the RFC
-  // calls it that)
-  u[8] = (u[8] & 0xbf) | 0x80
-
-  // hex encode them and add the dashes
-  var uid = "";
-  uid += u[0].toString(16);
-  uid += u[1].toString(16);
-  uid += u[2].toString(16);
-  uid += u[3].toString(16);
-  uid += "-";
-
-  uid += u[4].toString(16);
-  uid += u[5].toString(16);
-  uid += "-";
-
-  uid += u[6].toString(16);
-  uid += u[7].toString(16);
-  uid += "-";
-
-  uid += u[8].toString(16);
-  uid += u[9].toString(16);
-  uid += "-";
-
-  uid += u[10].toString(16);
-  uid += u[11].toString(16);
-  uid += u[12].toString(16);
-  uid += u[13].toString(16);
-  uid += u[14].toString(16);
-  uid += u[15].toString(16);
-
-  return uid;
+   // FIXME: not a proper uuid
+   let id = "";
+   while (id.length < 12) {
+     // 46656 == Math.pow(36, 3)
+     let num;
+     if (! id) {
+       num = Date.now() % 46656;
+     } else {
+       num = Math.floor(Math.random() * 46656);
+     }
+     id += num.toString(36);
+   }
+   return id;
 }
 
 class AbstractShot {
