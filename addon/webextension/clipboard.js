fa86bc1093d7bc0c47d711876d38e85627e07cb9
niharikak101 <niharikak101@gmail.com>
2017-03-21 15:11:27 +0000

    shows error when image is not saved

--- addon/webextension/clipboard.js
+++ addon/webextension/clipboard.js
@@ -4,14 +4,18 @@ window.clipboard = (function () {
   let exports = {};
 
   exports.copy = function (text) {
+    if(!text) {
+      catcher.unhandled(new Error("Screenshot could not be saved"));
+      return false;
+    }
     let el = document.createElement("textarea");
     document.body.appendChild(el);
     el.value = text;
     el.select();
-    const copied = document.execCommand("copy") && text;
+    const copied = document.execCommand("copy");
     document.body.removeChild(el);
     if (!copied) {
-      catcher.unhandled(new Error("clipboard copy failed"));
+      catcher.unhandled(new Error("Clipboard copy failed"));
     }
     return copied;
   };
