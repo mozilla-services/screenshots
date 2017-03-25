dca73fdbc8c5a6d1b105303e0cdbefd3675ba2b3
Cecile Bertin <elicec12@gmail.com>
2017-03-25 11:20:04 -0700

    small link error

--- server/src/footer-view.js
+++ server/src/footer-view.js
@@ -11,7 +11,7 @@ exports.Footer = class Footer extends React.Component {
           <div className="footer-right">
             <div className="legal-links">
               <a href="/terms" target="_blank">Terms</a>
-              <a href="https://www.mozilla.org/fr/privacy/websites/" target="_blank">Privacy Notice</a>
+              <a href="https://www.mozilla.org/privacy/websites/" target="_blank">Privacy Notice</a>
               <a href="https://www.mozilla.org/en-US/about/legal/report-infringement/" target="_blank">Report IP Infringement</a>
               <a href="https://discourse.mozilla-community.org/c/test-pilot/page-shot" target="_blank">Give Feedback</a>
               <a href="https://github.com/mozilla-services/screenshots" target="_blank">GitHub</a>
