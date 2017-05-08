f73928cadba36ada7ae0ed7b69fc63b8d51c6d72
Danny Coates <dannycoates@gmail.com>
2017-05-08 12:39:29 -0700

    use siteOrigin for /proxy urls

--- server/src/proxy-url.js
+++ server/src/proxy-url.js
@@ -2,7 +2,7 @@ const dbschema = require("./dbschema");
 const config = require("./config").getProperties();
 
 exports.createProxyUrl = function(req, url, hash) {
-  let base = `${req.protocol}://${config.contentOrigin}`;
+  let base = `${req.protocol}://${config.siteOrigin}`;
   let sig = dbschema.getKeygrip().sign(new Buffer(url, 'utf8'));
   let proxy = `${base}/proxy?url=${encodeURIComponent(url)}&sig=${encodeURIComponent(sig)}`;
   if (hash) {
