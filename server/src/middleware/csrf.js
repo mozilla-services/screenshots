61d944daeb9989caa154616e7f876e8745b46b25
Greg Guthe <gguthe@mozilla.com>
2017-08-29 12:38:42 -0400

    update csrf cookie config
    * remove samesite and host prefix
    * set secure attr when expecting https protocol

--- server/src/middleware/csrf.js
+++ server/src/middleware/csrf.js
@@ -3,20 +3,24 @@ const mozlog = require("../logging").mozlog("csrf-middleware");
 const { captureRavenException } = require("../ravenclient");
 const { simpleResponse } = require("../responses");
 
-exports.csrfProtection = csrf({cookie: {httpOnly: true, secure: true, sameSite: 'lax', key: '__Host-csrf'}});
+const config = require("../config").getProperties();
+
+const useSecureCsrfCookie = (config.expectProtocol && /^https$/.test(config.expectProtocol));
+
+exports.csrfProtection = csrf({cookie: {httpOnly: true, secure: useSecureCsrfCookie}});
 
 exports.csrf = function(req, res, next) {
   // The cookies library doesn't detect duplicates; check manually
   let rawCookies = req.get("cookie") || "";
   let pairs = rawCookies.split(";");
-  let csrfTokens = pairs.filter(item => item.match(/__Host-csrf=/));
+  let csrfTokens = pairs.filter(item => item.match(/_csrf=/));
   if (csrfTokens.length > 1) {
     let exc = new Error("Duplicate CSRF cookies");
     exc.headerValue = rawCookies;
     captureRavenException(exc);
     simpleResponse(res, "Bad request", 400);
   }
-  req.cookies._csrf = req.cookies.get("__Host-csrf"); // csurf expects a property
+  req.cookies._csrf = req.cookies.get("_csrf"); // csurf expects a property
   next();
 };
 
