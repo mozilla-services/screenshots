7544e9f6203b020ef813d3c2cf3e88927046044b
Ian Bicking <ianb@colorstudy.com>
2017-09-14 14:33:29 -0500

    Fix #3492, update favicon
    No 96x96 favicon was provided, so that file/link was removed.
    
    A SVG source was added for reference (but is not used)

--- server/src/reactruntime.js
+++ server/src/reactruntime.js
@@ -40,7 +40,6 @@ exports.HeadTemplate = class HeadTemplate extends React.Component {
         <link rel="shortcut icon" href={this.props.staticLink("/static/img/favicon-32.png")} />
         <link rel="icon" type="image/png" href={this.props.staticLink("/static/img/favicon-16.png")} sizes="16x16"/>
         <link rel="icon" type="image/png" href={this.props.staticLink("/static/img/favicon-32.png")} sizes="32x32"/>
-        <link rel="icon" type="image/png" href={this.props.staticLink("/static/img/favicon-96.png")} sizes="96x96"/>
         { analyticsScript }
         { activationScript }
         { this.props.sentryPublicDSN ? <script src={this.props.staticLink("/install-raven.js")} async /> : null }
