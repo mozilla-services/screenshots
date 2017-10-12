76fe5b006989678d70173bd83bcc2482d27a2a06
Barry Chen <chenba@gmail.com>
2017-10-12 18:04:09 -0500

    Set s-maxage for shared cache on shot images. (#3591)

--- server/src/caching.js
+++ server/src/caching.js
@@ -1,5 +1,6 @@
 const config = require("./config").getProperties();
 
+const tenMinutesInSeconds = 10 * 60;
 const aDayInSeconds = 24 * 60 * 60;
 exports.cacheTime = 60 * 60 * 24 * 30; // 30 days
 
@@ -7,12 +8,21 @@ if (!config.setCache) {
   exports.cacheTime = 0;
 }
 
-function createCacheSetter(cacheLength) {
+function createCacheSetter(maxAge, flags) {
   return function(res, options) {
-    if (config.setCache && cacheLength) {
+    if (config.setCache && maxAge) {
+      let vals = [];
       options = options || {};
+
       let pub = options.private ? "private" : "public";
-      res.set("Cache-Control", `${pub}, max-age=${cacheLength}`);
+      vals.push(pub);
+      vals.push(`max-age=${maxAge}`)
+
+      if (flags) {
+        vals = vals.concat(flags);
+      }
+
+      res.set("Cache-Control", vals.join(', '));
     } else {
       res.set("Cache-Control", "no-cache");
     }
@@ -20,4 +30,4 @@ function createCacheSetter(cacheLength) {
 }
 
 exports.setMonthlyCache = createCacheSetter(exports.cacheTime);
-exports.setDailyCache = createCacheSetter(aDayInSeconds);
+exports.setDailyCache = createCacheSetter(aDayInSeconds, `s-maxage=${tenMinutesInSeconds}`);
