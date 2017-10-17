c51912b6203efb7c568a57a4b1b5750de585607c
Apply55gx <Apply55gx@users.noreply.github.com>
2017-10-17 22:24:31 +0200

    Create empty robots.txt. Closes #3635 (#3639)
    * Create empty robots.txt. Closes #3635
    
    https://github.com/mozilla-services/screenshots/issues/3635
    
    * delete misplaced robots.txt
    
    * Create robots.txt
    
    * Delete robots.txt
    
    * Update Server.js
    
    add /robots.txt case

--- server/src/pages/homepage/server.js
+++ server/src/pages/homepage/server.js
@@ -5,6 +5,10 @@ let app = express();
 
 exports.app = app;
 
+app.get("/robots.txt", function(req, res) {
+  res.send('');
+});
+
 app.get("/", function(req, res) {
   const page = require("./page").page;
   reactrender.render(req, res, page);
