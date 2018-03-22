ba4073fafcf32e7489d6fe3993211268ac0136c6
Barry Chen <chenba@gmail.com>
2018-03-22 18:00:57 -0500

    Set display CSS property on iframes. (#4148) (#4256)

--- addon/webextension/selector/ui.js
+++ addon/webextension/selector/ui.js
@@ -173,7 +173,7 @@ this.ui = (function() { // eslint-disable-line no-unused-vars
 
     unhide() {
       this.updateElementSize();
-      this.element.style.display = "";
+      this.element.style.display = "block";
       catcher.watchPromise(callBackground("sendEvent", "internal", "unhide-selection-frame"));
       if (highContrastCheck(this.element.contentWindow)) {
         this.element.contentDocument.body.classList.add("hcm");
@@ -222,7 +222,7 @@ this.ui = (function() { // eslint-disable-line no-unused-vars
         }
       }
       if (force && visible) {
-        this.element.style.display = "";
+        this.element.style.display = "block";
       }
     },
 
@@ -344,7 +344,7 @@ this.ui = (function() { // eslint-disable-line no-unused-vars
     unhide() {
       window.addEventListener("scroll", watchFunction(assertIsTrusted(this.onScroll)));
       window.addEventListener("resize", this.onResize, true);
-      this.element.style.display = "";
+      this.element.style.display = "block";
       catcher.watchPromise(callBackground("sendEvent", "internal", "unhide-preselection-frame"));
       if (highContrastCheck(this.element.contentWindow)) {
         this.element.contentDocument.body.classList.add("hcm");
@@ -458,7 +458,7 @@ this.ui = (function() { // eslint-disable-line no-unused-vars
     },
 
     unhide() {
-      this.element.style.display = "";
+      this.element.style.display = "block";
       catcher.watchPromise(callBackground("sendEvent", "internal", "unhide-preview-frame"));
       this.element.focus();
     },
