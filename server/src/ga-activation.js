d0143c9958137ad45b12ef5c7418f66526a8b6ea
Jared Hirsch <ohai@6a68.net>
2018-12-10 15:27:07 -0800

    Discontinue server-side use of GA
    Rather than deleting code, for now, simply mark all visitors as having
    Do Not Track enabled.

--- server/src/ga-activation.js
+++ server/src/ga-activation.js
@@ -12,38 +12,8 @@ function _dntEnabled(dnt, ua) {
 
     'use strict';
 
-    // for old version of IE we need to use the msDoNotTrack property of navigator
-    // on newer versions, and newer platforms, this is doNotTrack but, on the window object
-    // Safari also exposes the property on the window object.
-    var dntStatus = dnt || navigator.doNotTrack || window.doNotTrack || navigator.msDoNotTrack;
-    var ua = ua || navigator.userAgent;
-
-    // List of Windows versions known to not implement DNT according to the standard.
-    var anomalousWinVersions = ['Windows NT 6.1', 'Windows NT 6.2', 'Windows NT 6.3'];
-
-    var fxMatch = ua.match(/Firefox\\/(\\d{1,10})/);
-    var ieRegEx = /MSIE|Trident/i;
-    var isIE = ieRegEx.test(ua);
-    // Matches from Windows up to the first occurance of ; un-greedily
-    // http://www.regexr.com/3c2el
-    var platform = ua.match(/Windows.+?(?=;)/g);
-
-    // With old versions of IE, DNT did not exist so we simply return false;
-    if (isIE && typeof Array.prototype.indexOf !== 'function') {
-        return false;
-    } else if (fxMatch && parseInt(fxMatch[1], 10) < 32) {
-        // Can't say for sure if it is 1 or 0, due to Fx bug 887703
-        dntStatus = 'Unspecified';
-    } else if (isIE && platform && anomalousWinVersions.indexOf(platform.toString()) !== -1) {
-        // default is on, which does not honor the specification
-        dntStatus = 'Unspecified';
-    } else {
-        // sets dntStatus to Disabled or Enabled based on the value returned by the browser.
-        // If dntStatus is undefined, it will be set to Unspecified
-        dntStatus = { '0': 'Disabled', '1': 'Enabled' }[dntStatus] || 'Unspecified';
-    }
-
-    return dntStatus === 'Enabled' ? true : false;
+    // Disable GA.
+    return true;
 }
 `;
 
