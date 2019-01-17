52bda289d787c8bf08a290273dd727d59f66750a
Ian Bicking <ian@ianbicking.org>
2019-01-17 09:49:51 -0600

    Metrics improvements (#5315)
    * Fix #5293, stop sending metrics if server isn't available
    
    Also adds logic to not send timing information if server is unavailable
    
    * Fix #5264, only send analytics for 10% of users
    
    This random generates a number for users when Screenshots is loaded, then saves that number, to determine who will actually send metrics.

--- webextension/background/analytics.js
+++ webextension/background/analytics.js
@@ -5,11 +5,17 @@
 this.analytics = (function() {
   const exports = {};
 
+  const GA_PORTION = 0.1; // 10% of users will send to the server/GA
+  // This is set from storage, or randomly; if it is less that GA_PORTION then we send analytics:
+  let myGaSegment = 1;
   let telemetryPrefKnown = false;
   let telemetryEnabled;
-  // If we ever get a 410 Gone response from the server, we'll stop trying to send events for the rest
+  // If we ever get a 410 Gone response (or 404) from the server, we'll stop trying to send events for the rest
   // of the session
   let hasReturnedGone = false;
+  // If there's this many entirely failed responses (e.g., server can't be contacted), then stop sending events
+  // for the rest of the session:
+  let serverFailedResponses = 3;
 
   const EVENT_BATCH_DURATION = 1000; // ms for setTimeout
   let pendingEvents = [];
@@ -21,6 +27,10 @@ this.analytics = (function() {
     headers: { "content-type": "application/json" },
     credentials: "include",
   };
+
+  function shouldSendEvents() {
+    return !hasReturnedGone && serverFailedResponses > 0 && myGaSegment < GA_PORTION;
+  }
 
   function flushEvents() {
     if (pendingEvents.length === 0) {
@@ -61,6 +71,9 @@ this.analytics = (function() {
   function sendTiming(timingLabel, timingVar, timingValue) {
     // sendTiming is only called in response to sendEvent, so no need to check
     // the telemetry pref again here.
+    if (!shouldSendEvents()) {
+      return;
+    }
     const timingCategory = "addon";
     pendingTimings.push({
       timingCategory,
@@ -113,7 +126,7 @@ this.analytics = (function() {
     for (const [gaField, value] of Object.entries(abTests)) {
       options[gaField] = value;
     }
-    if (hasReturnedGone) {
+    if (!shouldSendEvents()) {
       // We don't want to save or send the events anymore
       return Promise.resolve();
     }
@@ -315,17 +328,36 @@ this.analytics = (function() {
 
   function fetchWatcher(request) {
     request.then(response => {
-      if (response.status === 410) { // Gone
+      if (response.status === 410 || response.status === 404) { // Gone
         hasReturnedGone = true;
         pendingEvents = [];
+        pendingTimings = [];
       }
       if (!response.ok) {
         log.debug(`Error code in event response: ${response.status} ${response.statusText}`);
       }
     }).catch(error => {
+      serverFailedResponses--;
+      if (serverFailedResponses <= 0) {
+        log.info(`Server is not responding, no more events will be sent`);
+        pendingEvents = [];
+        pendingTimings = [];
+      }
       log.debug(`Error event in response: ${error}`);
     });
   }
+
+  async function init() {
+    const result = await browser.storage.local.get(["myGaSegment"]);
+    if (!result.myGaSegment) {
+      myGaSegment = Math.random();
+      await browser.storage.local.set({myGaSegment});
+    } else {
+      myGaSegment = result.myGaSegment;
+    }
+  }
+
+  init();
 
   return exports;
 })();
