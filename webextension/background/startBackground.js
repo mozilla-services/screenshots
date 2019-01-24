45607dd38d971ff86663ba5ad83940b5f56ee8f9
Ian Bicking <ianb@colorstudy.com>
2019-01-24 15:38:38 -0600

    Fix #5330, show context menu when there's a text selection

--- webextension/background/startBackground.js
+++ webextension/background/startBackground.js
@@ -50,7 +50,7 @@ this.startBackground = (function() {
   browser.contextMenus.create({
     id: "create-screenshot",
     title: browser.i18n.getMessage("contextMenuLabel"),
-    contexts: ["page"],
+    contexts: ["page", "selection"],
     documentUrlPatterns: ["<all_urls>", "about:reader*"],
   });
 
